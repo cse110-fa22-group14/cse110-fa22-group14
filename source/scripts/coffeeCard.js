3bf43a50568df00f0d28987f493366cc707c61e4
William <williammmm.kim@gmail.com>
2022-11-03 20:41:23 -0700

    added a js file for cofeeCard web component

---
+++ source/scripts/coffeeCard.js
@@ -0,0 +1,6 @@
+/* TODO: 
+ * coffee card should include date (Monday 12, 2022 for example)
+ * a share button to post the card to social media (this will be implemented later)
+ * Title of Drink
+ * Short Description of the drink: this content is still to be decided
+ */
