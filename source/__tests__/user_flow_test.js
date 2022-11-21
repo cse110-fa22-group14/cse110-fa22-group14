61b67990b9c1a3f22f4e312482c0faec9105838a
William <williammmm.kim@gmail.com>
2022-11-21 14:28:00 -0800

    adding first test

--- source/__tests__/user_flow_test.js
+++ source/__tests__/user_flow_test.js
@@ -1,8 +1,9 @@
 describe("Basic user flow for Website", () => {
-    // First, visit the lab 8 website
+    
     beforeAll(async () => {
-      await page.goto('http://127.0.0.1:5501/source/'); 
-    });
+      await page.goto('http://127.0.0.1:5501/source/');
+     });
+    
 
 
     // NOTE: The name and implementation of these tests are open to change. 
@@ -10,13 +11,27 @@ describe("Basic user flow for Website", () => {
     // But we should be thinking about testing for performance of our app 
 
 
-
-
     // Check to make sure that adding 10 cards to the page will create 10 cards
     it("Adding 10 cards should populate the gallery with 10 cards ", async () => {
   
       // In a for loop click on the add button, provide input, and save
+      const buttons = await page.$$("button");
+      console.log(buttons[3])
+      let id = await buttons[3].getProperty("id");
+      console.log(id)
+      
+      for(let id = 0; id < 10; id++) {
+        await addButton.click();
+        await page.$eval("str_drink_name", el => el.value = "name" + id );
+        await page.$eval("int_drink_price", el => el.value = "price" + id );
+        await page.$eval("time_purchase_date", el => el.value = "date" + id );
+        await page.$eval("str_purchase_location", el => el.value = "location" + id );
+        const saveButton = await page.$("input[type=submit");
+        await saveButton.click();
+      }
       // Then check the gallery to make sure there are 10 cards
+      const numCards = await page.$$("coffee-card");
+      expect(numCards.length).toBe(10);
 
     });
 
