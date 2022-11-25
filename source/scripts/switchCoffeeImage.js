3dd32eb74a508df59504514d6cfc8a225791f81d
hurl365 <35495052+hurl365@users.noreply.github.com>
2022-11-24 23:35:32 -0800

    Fixed Style for switchCoffeeImage.js

--- source/scripts/switchCoffeeImage.js
+++ source/scripts/switchCoffeeImage.js
@@ -1,58 +1,62 @@
 /**
  * @author Ruilin Hu and Yuang and William
  * @file - script to the image switching bottons
- * @version 0.0.1
+ * @version 0.0.2
  * @Created Nov 12, 2022
- * @Edited 
+ * @Edited Nov 24, 2022
  * referenc source: https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
  */
 
-//execute init when the contents in the window fully loaded
+// Execute init when the contents in the window fully loaded
 window.addEventListener('DOMContentLoaded', init);
-
-//the variable to hold the current image's index
+// The variable to hold the current image's index
 let coffee_picture_index = 0;
-//total number of coffee images in the asset/images folder
+// Total number of coffee images in the asset/images folder
 const TOTAL_COFFEE_PICTURE_CNT = 4;
-//the relative path to the pictures of the coffee images
+// The relative path to the pictures of the coffee images
 const COFFEE_PICTURE_PATH = "./assets/images/coffee-picture";
-//the extension of the coffee images file
+// The extension of the coffee images file
 const COFFEE_PICTURE_EXTENSION = ".png";
+// Next left image
+const LEFT_IMAGE = -1;
+// Next right image
+const RIGHT_IMAGE = -1;
+// Default image is the first one
+const DEFAULT_IMAGE_ID = 0;
 
 /**
- * This function will adjust the coffee image on the card based on the 
+ * This function will adjust the coffee image on the card based on the
  * user's control.
  * @param int_change - the change in the index of the coffee picture to display.
  * @return void
- */ 
+ */
 function next_image(int_change) {
    show_coffee_image(coffee_picture_index += int_change);
-   console.log('imaged changed to: ' + COFFEE_PICTURE_PATH + 
+   console.log('imaged changed to: ' + COFFEE_PICTURE_PATH +
         coffee_picture_index + COFFEE_PICTURE_EXTENSION);
 }
 
 /**
  * This function will return the current coffee_picture_index.
  * @return coffee_picture_index, the current index of the coffee picture
- */ 
-function get_image_id() { 
+ */
+function get_image_id() {
     return coffee_picture_index;
 }
 
 /**
  * This function will return the current coffee_picture_index.
  * @return void
- */ 
+ */
  function reset_image_id() {
-    coffee_picture_index = 0;
+    coffee_picture_index = DEFAULT_IMAGE_ID;
 }
 
- 
 /**
  * This function will set specific coffee image directly.
  * @param int_change - the index of the coffee picture to display on the current card.
  * @return void
- */ 
+ */
 function set_image(int_change) {
    show_coffee_image(coffee_picture_index = int_change);
 }
@@ -60,36 +64,39 @@ function set_image(int_change) {
 /**
  * This function will find the coffee image's element on the html and change
  * its src to the corresponding coffee's image.
- * @param int_change - the index of the coffee picture to display on 
+ * @param int_change - the index of the coffee picture to display on
  * the current card.
  * @return void
- */ 
+ */
 function show_coffee_image(int_change) {
     if (int_change >= TOTAL_COFFEE_PICTURE_CNT) {
-        coffee_picture_index = 0;
-        //change to the first picture if index increases out of bounds
+        coffee_picture_index = DEFAULT_IMAGE_ID;
+        // Change to the first picture if index increases out of bounds
     }
-    if (int_change < 0) {
-        coffee_picture_index = TOTAL_COFFEE_PICTURE_CNT - 1;
-        //change to the last picture is index decreases out of bounds
+    if (int_change < DEFAULT_IMAGE_ID) {
+        coffee_picture_index = TOTAL_COFFEE_PICTURE_CNT + LEFT_IMAGE;
+        // Change to the last picture is index decreases out of bounds
     }
-    //find the image element on html
+    // Find the image element on html
     const coffee_picture_element = document.getElementById('img_drink_image');
-    //change the src of the image
-    coffee_picture_element.src = 
+    // Change the src of the image
+    coffee_picture_element.src =
         COFFEE_PICTURE_PATH + coffee_picture_index + COFFEE_PICTURE_EXTENSION;
  }
 
- //initialize things when the window loads
+ // Initialize things when the window loads
 function init(){
-    //find the button elements
-    let switch_photo_left_arrow = document.getElementById('switch_photo_left_arrow');
-    let switch_photo_right_arrow = document.getElementById('switch_photo_right_arrow');
-    //attach event listeners to the arrow anchors
+    // Find the button elements
+    set_image(coffee_picture_index);
+    const switch_photo_left_arrow = document.getElementById('switch_photo_left_arrow');
+    const switch_photo_right_arrow = document.getElementById('switch_photo_right_arrow');
+    // Attach event listeners to the arrow anchors
     switch_photo_left_arrow.addEventListener("click", function() {
-        next_image(-1);
+        next_image(LEFT_IMAGE);
     });
     switch_photo_right_arrow.addEventListener("click", function() {
-        next_image(1);
+        next_image(RIGHT_IMAGE);
     });
+    get_image_id();
+    reset_image_id();
 }
