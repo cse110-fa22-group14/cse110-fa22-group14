e6d7c67b4289a30571f4bee31b28637f00687d2e
hurl365 <35495052+hurl365@users.noreply.github.com>
2022-12-03 21:08:06 -0800

    Adding help texts

--- source/scripts/guidance.js
+++ source/scripts/guidance.js
@@ -20,10 +20,45 @@ const NEXT_TEXT = 1;
 // Default image is the first one
 const DEFAULT_TEXT_INDEX = 0;
 
-const help_text = ['text_0', 'text_1', 'text_2', 'text_3'];
+const help_text = [];
+
+// Step 0 text
+help_text.push("Hello! Welcome to Brew Notes by ProForce. It is our pleasure to have "
+                + "you on board. Let's take a short tour of your new Coffee Cards! Please "
+                + "click the glowing black arrow on the right side of the screen to continue with the tutorial.");
+// Step 1 text
+help_text.push("To end the tutorial at any time, feel free to click the back button to go back to "
+                + "the app or click the [main page] button.");
+// Step 2 text
+help_text.push("To Create a new Coffee Card, use the plus button and open up the edit page.");
+// Step 3 text
+help_text.push("Please fill in the details of the coffee to record, drag the sliders to adjust their values, "
+                + "and click on the drop down selectors to change value.");
+// Step 4 text
+help_text.push("When the coffee card is ready to save, simply click the \"SAVE\" button to add it to the gallery.");
+// Step 5 text
+help_text.push("A new card has just been added to the gallery! The card's thumnail will display some useful "
+                + "information about the card for easier access.");
+// Step 6 text
+help_text.push("In order to make edits to the card we just added, simply click the \"Edit\" button to open up the details page.");
+// Step 7 text
+help_text.push("Make any changes to the card if necessary and save it in the same way when we first added the card to the gallery. "
+                + "However, if we want to discard the changes, simply click the \"CANCEL\" button instead to revert the changes "
+                + "we made on the edit page.");
+// Step 8 text
+help_text.push();
+// Step 10 text
+help_text.push();
+// Step 11 text
+help_text.push();
+// Step 12 text
+help_text.push();
+// Step 13 text
+help_text.push();
+// Step 14 text
+help_text.push();
 
 // Executes when the window loads
-
 function init() {
     help_text_index = 0;
     handleEvents();
@@ -58,6 +93,106 @@ function show_help_text(int_change) {
     const help_text_element = document.getElementById('str_guidance_text');
     // Change the src of the image
     help_text_element.innerText = help_text[help_text_index];
+    // Change the element glow
+    glowGuidanceElement(help_text_index);
+}
+
+/**
+ * This function will adjust the glow effect of each element according to
+ * the current help text being displayed.
+ * @param int_help_index - the index currently displayed help text
+ * @return void
+ */
+function glowGuidanceElement(int_help_index) {
+    const dropBox = document.querySelector("body");
+    dropBox.classList.add('disable-css-transitions');
+    const help = document.getElementById("help");
+    help.classList.add('disable-css-transitions');
+    const filterOption = document.getElementById("filter");
+    filterOption.classList.add('disable-css-transitions');
+    const sortSelect = document.getElementById("filter");
+    sortSelect.classList.add('disable-css-transitions');
+    const color_picker = document.getElementById("change_color");
+    color_picker.classList.add('disable-css-transitions');
+    const fileSelectButton = document.getElementById("select_file");
+    fileSelectButton.classList.add('disable-css-transitions');
+    const importButton = document.getElementById("import");
+    importButton.classList.add('disable-css-transitions');
+    const addButton = document.getElementById('add_card');
+    addButton.classList.add('disable-css-transitions');
+    const cancelButton = document.getElementById('cancel');
+    cancelButton.classList.add('disable-css-transitions');
+    const help_left_arrow = document.getElementById('prev_help');
+    help_left_arrow.classList.add('disable-css-transitions');
+    const help_right_arrow = document.getElementById('next_help');
+    help_right_arrow.classList.add('disable-css-transitions');
+    
+    switch(int_help_index) {
+        case 11:
+            filterOption.classList.remove('disable-css-transitions');
+            break
+        default:
+            throw new Error('Invalid message index!');
+    }
+    
+}
+
+function handleEvents() {
+    // Find the button elements
+    show_help_text(DEFAULT_TEXT_INDEX);
+    const help_left_arrow = document.getElementById('prev_help');
+    const help_right_arrow = document.getElementById('next_help');
+    // Attach event listeners to the arrow anchors
+    help_left_arrow.addEventListener("click", function() {
+        next_text(PREV_TEXT);
+    });
+    help_right_arrow.addEventListener("click", function() {
+        next_text(NEXT_TEXT);
+    });
+}
+
+function openForm() {
+    const form = document.getElementById('pop_up_box');
+    form.style.opacity = ONE;
+    form.style.visibility = "visible";
+    isFormOpen = true;
+}
+
+function closeForm() {
+    const form = document.getElementById('pop_up_box');
+    form.style.opacity = 0;
+    form.style.visibility = "hidden";
+
+    // Reset the form's html contents when done.
+    document.getElementById("str_drink_name").value = "";
+    document.getElementById("float_drink_price").value = "";
+    document.getElementById("time_purchase_date").value = "";
+    document.getElementById("str_purchase_location").value = "";
+
+    // Populate the slider's display value
+    document.getElementById("acidity_val").innerText = "0";
+    document.getElementById("sweetness_val").innerText = "0";
+    document.getElementById("bitterness_val").innerText = "0";
+    document.getElementById("saltiness_val").innerText = "0";
+
+    // Change slider value
+    document.getElementById("int_slide_acidity").value = 0;
+    document.getElementById("int_slide_sweetness").value = 0;
+    document.getElementById("int_slide_bitterness").value = 0;
+    document.getElementById("int_slide_saltiness").value = 0;
+
+    // Populate the dropdowns
+    document.getElementById("str_drink_type").value = "Casual";
+    document.getElementById("str_brew_style").value = "Drip";
+    document.getElementById("int_dropdown_color").value = "Light";
+    document.getElementById("str_notes").value = "";
+
+    // Set the coffee card's image using the function in switchCoffeeImages.js
+    document.getElementById("bool_check_chocolate").checked = false;
+    document.getElementById("bool_check_caramel").checked = false;
+    document.getElementById("bool_check_nutty").checked = false;
+    document.getElementById("bool_check_fruity").checked = false;
+    isFormOpen = false;
 }
 
 /**
@@ -69,7 +204,6 @@ function show_help_text(int_change) {
  * @param {Array<Object>} coffeeCards An array of recipes
  */
 function addCoffeeCardsToDocument(coffeeCards) {
-
 
     if (!coffeeCards) {
         return;
@@ -93,95 +227,4 @@ function addCoffeeCardsToDocument(coffeeCards) {
         coffeeCard.id = index;
         gallery.appendChild(coffeeCard);
     })
-
-    // Make sure to keep the style consistent
-    const cards = document.querySelectorAll("coffee-card");
-    const theme = localStorage.getItem("theme");
-    switch_theme(cards, JSON.parse(theme));
-
-
 }
-
-function handleEvents() {
-
-    // Define variables to hold DOM elements
-    const dropBox = document.querySelector("body");
-    // TODO: const helpButton = document.getElementById("help");
-    const help = document.getElementById("help")
-    const filterOption = document.getElementById("filter");
-    const sortSelect = document.getElementById("filter");
-
-    const color_picker = document.getElementById("change_color");
-    const fileSelectButton = document.getElementById("select_file");
-    const importButton = document.getElementById("import");
-
-    const addButton = document.getElementById('add_card');
-    const form = document.getElementById('pop_up_box');
-    const cancelButton = document.getElementById('cancel');
-    const flavorSliders = document.getElementsByClassName('flavor_range');
-
-    let isEditing = false;
-    let isFormOpen = false;
-
- 
-    importButton.style.opacity = 0;
-
-    // Find the button elements
-    show_help_text(DEFAULT_TEXT_INDEX);
-    const help_left_arrow = document.getElementById('prev_help');
-    const help_right_arrow = document.getElementById('next_help');
-    // Attach event listeners to the arrow anchors
-    help_left_arrow.addEventListener("click", function() {
-        next_text(PREV_TEXT);
-    });
-    help_right_arrow.addEventListener("click", function() {
-        next_text(NEXT_TEXT);
-    });
-
-    function openForm() {
-        form.style.opacity = ONE;
-        form.style.visibility = "visible";
-        isFormOpen = true;
-    }
-
-    function closeForm() {
-
-        form.style.opacity = 0;
-        form.style.visibility = "hidden";
-
-
-        // Reset the form's html contents when done.
-        document.getElementById("str_drink_name").value = "";
-        document.getElementById("float_drink_price").value = "";
-        document.getElementById("time_purchase_date").value = "";
-        document.getElementById("str_purchase_location").value = "";
-
-        // Populate the slider's display value
-        document.getElementById("acidity_val").innerText = "0";
-        document.getElementById("sweetness_val").innerText = "0";
-        document.getElementById("bitterness_val").innerText = "0";
-        document.getElementById("saltiness_val").innerText = "0";
-
-        // Change slider value
-        document.getElementById("int_slide_acidity").value = 0;
-        document.getElementById("int_slide_sweetness").value = 0;
-        document.getElementById("int_slide_bitterness").value = 0;
-        document.getElementById("int_slide_saltiness").value = 0;
-
-        // Populate the dropdowns
-        document.getElementById("str_drink_type").value = "Casual";
-        document.getElementById("str_brew_style").value = "Drip";
-        document.getElementById("int_dropdown_color").value = "Light";
-        document.getElementById("str_notes").value = "";
-
-        // Set the coffee card's image using the function in switchCoffeeImages.js
-        set_image(ZERO);
-        document.getElementById("bool_check_chocolate").checked = false;
-        document.getElementById("bool_check_caramel").checked = false;
-        document.getElementById("bool_check_nutty").checked = false;
-        document.getElementById("bool_check_fruity").checked = false;
-        isFormOpen = false;
-
-    }
-}
-
