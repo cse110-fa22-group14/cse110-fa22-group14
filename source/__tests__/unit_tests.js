d2ae6de0b7b651492ba12c1bf2a9505b36692fd0
hurl365 <35495052+hurl365@users.noreply.github.com>
2022-12-09 21:54:48 -0800

    Added Message

--- source/__tests__/unit_tests.js
+++ source/__tests__/unit_tests.js
@@ -2,7 +2,7 @@
  * This script is used to complete Unit tests to functions
  * Use global [function name] to declare functions and suppress ESLint warnings
  * Use const sum = require('../code-to-unit-test/sum.js'); to link to file
- * Commented by Ruilin Hu
+ * Commented by Ruilin Hu on Dec 9th, 2022
  */
 
 /* global test, expect */
